f48b4097089465b011c2dceebac13290dd0a9b07
Ivan Khoo <ivanakhoo@gmail.com>
2024-10-23 14:18:44 -0400

    Allowed other addresses

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def download_file(filename):
     return send_file(os.path.join(app.config['PROCESSED_FOLDER'], filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
